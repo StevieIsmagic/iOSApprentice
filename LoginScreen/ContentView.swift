18f1a79466fe2355b5498136c5ada9dd7c14a690
stevieismagic <stevieismagic@gmail.com>
2020-04-03 23:31:52 -0400

    init chapter 3

--- LoginScreen/ContentView.swift
+++ LoginScreen/ContentView.swift
@@ -9,22 +9,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Properties
+    // ==========
+    
+    // User interface views
+    @State var alertIsVisible: Bool = false
+    
+    // User interface content and layout
     var body: some View {
-        
-            VStack {
-                Text("Hello there")
-                    .fontWeight(.black)
-                    .foregroundColor(.green)
-                Button(action: {
-                    print("You logged in =)")
-                }) {
-                    Text("PRESS ME BUTTONS")
-                }
+        VStack {
+            Spacer()
+            
+            // Target row
+            HStack {
+                Text("How you feelin today ?")
+                Text("one hunnerd")
             }
             
+            Spacer()
+    
+            // Slider row
+            // Added views for the slider row here
+            HStack {
+                Text("1")
+                Slider(value: /*@START_MENU_TOKEN@*/.constant(10)
+/*@END_MENU_TOKEN@*/)
+                Text("100")
+            }
+            
+            Spacer()
+            
+            // Button row
+            Button(action: {
+                print("You console.logged me =)")
+                self.alertIsVisible = true
+            }) {
+                Text("HIT ME ?")
+            }
+            .alert(isPresented: self.$alertIsVisible) {
+                Alert(title: Text("Hello there !"),
+                      message: Text("My first Alert pop-up!"),
+                      dismissButton: .default(Text("Awesome!")))
+            }
+            
+            Spacer()
+            
+            // Score row
+            // Added views for the score, rounds, and start / info buttons here
+            HStack {
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score: ")
+                Text("99999")
+                Spacer()
+                Text("Round: ")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+        }
+        
     }
+    // Methods
+    // =======
 }
 
+// Preview
+// =======
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
